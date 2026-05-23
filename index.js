1d195835cac16e645d443cb4bf31ef2faa6f54df
javascript-system <javascript.system@gmail.com>
2026-05-23 08:17:23 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Backend online!");
+});
+
+app.get("/player/:tag", async (req, res) => {
+  try {
+    const tag = req.params.tag.replace("#", "");
+
+    const response = await fetch(
+      `https://api.brawlstars.com/v1/players/%23${tag}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.BRAWL_API_KEY}`
+        }
+      }
+    );
+
+    const data = await response.json();
+
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({
+      error: "Error when searching the player"
+    });
+  }
+});
+
+app.listen(process.env.PORT || 3000);
