40aafa3e14dfb31401c9e3c9f4c2285f729b6104
javascript-system <javascript.system@gmail.com>
2026-05-24 19:11:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -63,6 +63,55 @@ app.get(
   }
 );
 
+/* BATTLELOG */
+app.get(
+  "/battlelog/:tag",
+
+  async (req, res) => {
+
+    try {
+
+      const tag =
+        req.params.tag
+          .replace("#", "");
+
+      const response =
+        await fetch(
+
+`https://api.brawlstars.com/v1/players/%23${tag}/battlelog`,
+
+        {
+
+          headers: {
+
+            Authorization:
+              `Bearer ${process.env.BRAWL_API_KEY}`
+
+          }
+
+        }
+
+      );
+
+      const data =
+        await response.json();
+
+      res.json(data);
+
+    } catch {
+
+      res.status(500).json({
+
+        error:
+          "Battlelog failed"
+
+      });
+
+    }
+
+  }
+);
+
 /* CLUB */
 
 app.get(
